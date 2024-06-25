e8961639501d12959aa5c292878522f8555f6ab3
Aung Kyaw Khaing <76258097+iPanda007@users.noreply.github.com>
2024-06-25 21:35:10 +0630

    fix: position click

--- src/components/ui/pagination-ui/index.tsx
+++ src/components/ui/pagination-ui/index.tsx
@@ -71,7 +71,7 @@ const PaginationUi = ({
                   <div
                     key={index}
                     className={`cursor-pointer w-[20px]    text-[12px] relative  rounded-md px-4 flex items-center justify-center`}
-                    onClick={changePage}
+                
                   >
                     {item == currentPage && (
                       <MagicTabSelect id="pillTabs" transition={{ type: 'spring', bounce: 0.35 }}>
@@ -90,9 +90,10 @@ const PaginationUi = ({
                       </MagicTabSelect>
                     )}
                     <span
+                      onClick={changePage}
                       className={`${
                         item === currentPage ? 'text-white delay-150' : 'text-black'
-                      }  inline-flex h-[35px] items-center relative z-[2] transition-all duration-200 ease-in-out`}
+                      }  inline-flex h-[35px] w-full justify-center items-center relative z-[2] transition-all duration-200 ease-in-out`}
                     >
                       {item}
                     </span>
