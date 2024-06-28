56217bd2b6781f608a67a3aabb5669a23cb119d0
yawaianbuchi <yawaiaung.anbuchi@gmail.com>
2024-06-28 15:39:29 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,23 @@
 import { i18nRouter } from 'next-i18n-router';
 import i18nConfig from '../i18nConfig';
-import { NextRequest, NextResponse } from 'next/server';
-import { SessionData, sessionOptions } from './lib/session-options';
-import { getIronSession } from 'iron-session';
-import { cookies } from 'next/headers';
+import { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+  // const session = await getIronSession<SessionData>(cookies(), sessionOptions);
 
-  const isLoggedIn = session.isLoggedIn;
+  // const isLoggedIn = session.isLoggedIn;
 
-  if (isLoggedIn) {
-    if (pathname.match(/^\/[a-z]{2}\/dashboard$/)) {
-      return Response.redirect(new URL(`/`, request.url));
-    }
+  // if (isLoggedIn) {
+  if (pathname.match(/^\/[a-z]{2}\/dashboard$/)) {
+    return Response.redirect(new URL(`/`, request.url));
   }
-  if (!isLoggedIn) {
-    if (pathname === '/') {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
-  }
+  // }
+  // if (!isLoggedIn) {
+  //   if (pathname === '/') {
+  //     return NextResponse.redirect(new URL('/login', request.url));
+  //   }
+  // }
 
   // if (pathname === '/') {
   //   return Response.redirect(new URL(`/login`, request.url));
